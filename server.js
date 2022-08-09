1926d0ce7ead292320972d4fd31cc330955181bf
OlaniyiHope <olaniyihoppee@gmail.com>
2022-08-10 00:50:07 +0100

    all

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 5000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
+  '/http://localhost:5000/blogs/*': '/$1',
 }))
 server.use(router);
 server.listen(PORT, () => {
